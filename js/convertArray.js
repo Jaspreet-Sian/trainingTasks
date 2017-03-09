a228f261ac797079df28b036b071c75ff8450ccd
JasSian_972 <jasspreet.syan@gmail.com>
2017-03-09 23:24:15 +0530

    font pairing

--- js/convertArray.js
+++ js/convertArray.js
@@ -1,6 +1,29 @@
-function toArray(){
-  var img= document.getElementById("upldImg").value;
-  
-  console.log(img.width);
-  console.log(img.height);
-}
+
+
+  var toArray = function() {
+    var img= document.getElementById("image");
+    console.log(img);
+    // document.getElementById("arrayResult").appendChild(myImg);
+    var canvas= document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    console.log(canvas.width,"ww");
+    var context= canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    img.crossOrigin = 'anonymous';
+    var Data = context.getImageData(0,0,canvas.width,canvas.height).data;
+    var i=0, getBit=[];
+    console.log(Data.length/4);
+    console.log(Data);
+    for(i=0; i< Data.length; i+=4){
+      if(Data[i] >= 128){
+        getBit.push("1");
+      }
+      else{
+        getBit.push("0");
+      }
+    }
+
+    console.log(getBit.length,"kkk");
+    document.getElementById("arrayResult").innerHTML= getBit;
+  }
