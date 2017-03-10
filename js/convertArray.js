62568540f53f282477aadb89aff2cb0d0cfef6e3
JasSian_972 <jasspreet.syan@gmail.com>
2017-03-10 15:10:26 +0530

    image conversion

--- js/convertArray.js
+++ js/convertArray.js
@@ -8,22 +8,14 @@
     canvas.width = img.width;
     canvas.height = img.height;
     let context= canvas.getContext('2d');
-    context.drawImage(img, 0, 0);
-    img.crossOrigin = 'anonymous';
+    context.drawImage(img, 0, 0,canvas.width,canvas.height);
+    canvas.crossOrigin = 'anonymous';
     let Data = context.getImageData(0,0,canvas.width,canvas.height).data;
     var getBit=[];
     var intensityArr=[], intensityBitArr=[], intensityBit;
     // let i=0;
     console.log(Data);
     for(let i=0; i< Data.length; i+=4){
-      // White Bits
-      if(Data[i] >= 128){
-        getBit.push("1");
-      }
-      // Black bits
-      else{
-        getBit.push("0");
-      }
       // Luminisity method to find intesity
        let r = Data[i]/255;
        let g = Data[i + 1]/255;
