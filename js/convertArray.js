c22c3f5d8d7ff31f4a02e6e0a37932cb4fd05991
JasSian_972 <jasspreet.syan@gmail.com>
2017-03-10 15:00:28 +0530

    image conversion

--- js/convertArray.js
+++ js/convertArray.js
@@ -12,7 +12,7 @@
     img.crossOrigin = 'anonymous';
     let Data = context.getImageData(0,0,canvas.width,canvas.height).data;
     var getBit=[];
-    var intensityArr=[];
+    var intensityArr=[], intensityBitArr=[], intensityBit;
     // let i=0;
     console.log(Data);
     for(let i=0; i< Data.length; i+=4){
@@ -31,14 +31,17 @@
        let intensity = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
       intensity= intensity.toFixed(1);
       if(intensity >= 0.5){
-        intensity = Math.ceil(intensity);
+        intensityBit = Math.ceil(intensity);
       }
       else{
-        intensity = Math.floor(intensity);
+        intensityBit = Math.floor(intensity);
       }
       intensityArr.push(intensity);
+      intensityBitArr.push(intensityBit);
     }
     console.log(intensityArr);
-
-    document.getElementById("arrayResult").innerHTML= getBit;
+    // document.getElementById("res").setAttribute("style"," ");
+    document.getElementById("res").setAttribute("style"," display : flex; ");
+    document.getElementById("bitResult").innerHTML= intensityBitArr;
+    document.getElementById("intensityResult").innerHTML= intensityArr;
   }
